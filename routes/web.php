296c814b850ef2a33d7b94b9de51a51cdd1b03ff
josuengoma <ngomajosue80@gmail.com>
2023-04-17 14:19:12 +0100

    back api

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [MessageController::class, 'index'])->middleware(['auth'])->name('dashboard');
+
+
+Route::resource('users', User::class);
+
+Route::resource('messages', MessageController::class);
+
+require __DIR__ . '/auth.php';
